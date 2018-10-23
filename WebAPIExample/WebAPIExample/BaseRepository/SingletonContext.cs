6b567e53d697c0c3659d686cae8a7d02088dce92
Rafael Stunitz <rafaelstunitz@gmail.com>
2018-10-23 16:17:06 -0300

    Base Repository

---
+++ WebAPIExample/WebAPIExample/BaseRepository/SingletonContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebAPIExample.Models;
+
+namespace WebAPIExample.BaseRepository
+{
+    public class SingletonContext
+    {
+        private static Context ctx;
+        private SingletonContext() { }
+
+        public static Context GetInstance()
+        {
+            if (ctx == null)
+            {
+                ctx = new Context();
+            }
+            return ctx;
+        }
+    }
+}
